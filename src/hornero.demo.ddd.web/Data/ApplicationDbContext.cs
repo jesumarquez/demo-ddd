5fee74561f999255d204972b1f195090ef525be0
Jesú Márquez <jesumarquez@gmail.com>
2023-11-02 09:01:49 -0300

    Crear formulario de registro de cliente (#10)

---
+++ src/hornero.demo.ddd.web/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace hornero.demo.ddd.web.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
